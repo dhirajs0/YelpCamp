213186de2e33cf8d06bf02d051a851387784c701
Dhiraj Kumar Sah <dhirajsahcode@gmail.com>
2021-06-17 16:02:08 +0530

    app.js refactored with additional require

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
-const methodOverride = require('method-override');
-const Campground = require('./models/campground');
+const { campgroundSchema } = require('./schemas.js');
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
+const methodOverride = require('method-override');
+const Campground = require('./models/campground');
+
+
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp');
 
@@ -17,6 +20,9 @@ db.once("open", ()=>{
 
 
 const app = express();
+app.engine('ejs', ejsMate);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
@@ -31,9 +37,7 @@ const validateCampground = (req, res, next) => {
     }
 }
 
-app.engine('ejs', ejsMate);
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+
 
 
 app.get('/', (req, res) => {
